541391210ed107c6e047837163c8fd91cf25eb15
YannHyaric <ylhalmeida@gmail.com>
2020-10-28 11:15:02 -0300

    AlunoController Create

--- api/src/app/controllers/AlunoController.js
+++ api/src/app/controllers/AlunoController.js
@@ -2,7 +2,7 @@ import Aluno from '../models/Aluno';
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll()
+    const alunos = await Aluno.findAll();
     res.json(alunos);
   }
 
@@ -11,7 +11,18 @@ class AlunoController {
   }
 
   async create(req, res) {
-    // TODO
+    const { nome, email, cep, cidade, estado } = req.body;
+    const aluno = req.headers.authorization;
+
+    const [id_aluno] = await conect('incidents').insert({
+      nome,
+      email,
+      cep,
+      cidade,
+      estado,
+      id_aluno
+    });
+    return response.json({ id });
   }
 
   async update(req, res) {
